93da38e34f83718f435125e71b91e09299d5828c
Russell Hay <me@russellhay.com>
2019-05-05 09:00:27 -0700

    Happy path basic implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
+#[derive(PartialEq, Debug)]
+pub struct Generator {}
+#[derive(PartialEq, Debug)]
+pub struct MinimalId {}
+
+impl Default for Generator {
+	fn default() -> Self { Generator {} }
+}
+
+impl Default for MinimalId {
+	fn default() -> Self { MinimalId {} }
+}
+
+impl Generator {
+	pub fn generate(&self) -> MinimalId { MinimalId::default() }
+	pub fn id_from_str(&self, _id_str: &str) -> MinimalId { MinimalId::default() }
+}
+
+impl MinimalId {
+	pub fn to_string(&self) -> String { "".to_string() }
+
+	pub fn to_slice(&self) -> &[u8] { &[0] }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+	use super::*;
+
+	#[test]
+	fn acceptance_test_round_trip() {
+		let generator = Generator::default();
+		let id = generator.generate();
+		let id_str = id.to_string();
+		let id_int = id.to_slice();
+		let actual = generator.id_from_str(&id_str);
+		let actual_int = actual.to_slice();
+		assert_eq!(id, actual);
+		assert_eq!(id_int, actual_int);
+	}
 }
