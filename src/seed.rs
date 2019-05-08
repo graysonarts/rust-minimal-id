851cfc44158260244c7f452fa80f118082f72b16
Russell Hay <me@russellhay.com>
2019-05-07 17:07:08 -0700

    Fixes #2 to generate the seed from the timestamp

--- src/seed.rs
+++ src/seed.rs
@@ -1,6 +1,8 @@
 use byteorder::{NetworkEndian, WriteBytesExt};
+use std::time::SystemTime;
 
 /// Type representing the seed value
+#[derive(Debug)]
 pub struct Seed {
 	value: u32,
 }
@@ -10,14 +12,31 @@ impl Seed {
 	pub fn new(value: u32) -> Self { Self { value } }
 
 	/// Creates a new seed based on the current time
-	// TODO(#2): Implement generating the seed from the timestamp
-	pub fn from_time() -> Self { Self { value: 0 } }
+	///
+	/// ```
+	/// # use minimal_id::Seed;
+	/// let seed = Seed::from_time();
+	/// println!("Seed = {:?}", seed);
+	/// ```
+	pub fn from_time() -> Self {
+		let year_start = SystemTime::UNIX_EPOCH;
+		Self {
+			value: get_seconds_since(year_start),
+		}
+	}
 
 	/// Returns the seed as a byte array
 	pub fn as_slice(&self) -> [u8; 4] {
 		// TODO(#1): Force to Network Byte Order
 		unsafe { std::mem::transmute::<u32, [u8; 4]>(self.value) }
 	}
+}
+
+fn get_seconds_since(anchor: SystemTime) -> u32 {
+	let duration = SystemTime::now()
+		.duration_since(anchor)
+		.expect("Unable to calculate duration");
+	duration.as_secs() as u32
 }
 
 #[cfg(test)]
